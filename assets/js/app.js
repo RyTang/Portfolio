bad5dad26d087573d0940eac36206de325be76d5
Ryan Tang Li Jian <87563782+RyTang@users.noreply.github.com>
2023-02-20 20:04:44 +0800

    Updated Layout and bugs

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,6 +22,7 @@ const navLink = document.querySelectorAll(".nav-link");
 navLink.forEach((link) =>
     link.addEventListener("click", () => {
         ul.classList.remove("show");
+        burger.checked = false;
     })
 );
 
